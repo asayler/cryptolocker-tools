672f2750a531ed1b98f2f2b321b42aaf189c07dc
Andy Sayler <andy.sayler@gmail.com>
2014-12-08 18:15:44 -0700

    Added crypto check

--- mimes/default.py
+++ mimes/default.py
@@ -3,6 +3,8 @@ import mimetypes
 
 _ENCODING = "utf-8"
 
+_MGC_ENCRYPTED = 'application/octet-stream'
+
 def get_info(file_paths):
 
     infos = {}
@@ -14,6 +16,15 @@ def get_info(file_paths):
         mgc = magic.from_file(file_path, mime=True).decode(_ENCODING)
         info['mime'] = typ
         info['magic'] = mgc
+        if (mgc == _MGC_ENCRYPTED):
+            if (typ == None):
+                info['encrypted'] = "unknown"
+            elif (typ == mgc):
+                info['encrypted'] = "no"
+            else:
+                info['encrypted'] = "maybe"
+        else:
+            info['encrypted'] = "no"
         infos[file_path] = info
 
     return infos
